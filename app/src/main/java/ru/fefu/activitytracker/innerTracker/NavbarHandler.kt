8c8998c8a2e7f8e3fe024face29c07914862b1df
Welfegor <46806143+Welfegor@users.noreply.github.com>
2021-11-26 17:56:45 +1000

    Initial commit (Raw layout)
    Layouts for users_task, my_task, task_time, users_task_info, my_task_info

---
+++ app/src/main/java/ru/fefu/activitytracker/innerTracker/NavbarHandler.kt
@@ -0,0 +1,46 @@
+package ru.fefu.activitytracker.innerTracker
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import ru.fefu.activitytracker.R
+
+
+class NavbarHandler(private val fragments: List<TabFragment>, private val fragmentManager: FragmentManager) {
+    private lateinit var _hiddenMetaFragment : TabFragment
+
+    fun switchFragments(clickedButtonId: Int) {
+        val activeFragment = fragmentManager.fragments.firstOrNull { !it.isHidden }
+
+        _hiddenMetaFragment = fragments.filter { it.button_id == clickedButtonId }[0]
+        val hiddenFragment = fragmentManager.findFragmentByTag(_hiddenMetaFragment.tag)
+
+        if (activeFragment == hiddenFragment) return
+
+        if (hiddenFragment == null) {
+            fragmentManager.beginTransaction().apply {
+                add(
+                    R.id.fragment_view_tracker,
+                    _hiddenMetaFragment.fragment,
+                    _hiddenMetaFragment.tag
+                )
+                commit()
+            }
+        }
+        else {
+            fragmentManager.beginTransaction().apply {
+                show(hiddenFragment)
+                commit()
+            }
+        }
+
+        if (activeFragment != null) {
+            fragmentManager.beginTransaction().apply {
+                hide(activeFragment)
+                commit()
+            }
+        }
+
+    }
+}
+
+class TabFragment(val button_id: Int, val fragment: Fragment, val tag: String)
