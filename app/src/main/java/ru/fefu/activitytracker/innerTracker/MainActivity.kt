ff45c0b4df4b83da9924266b1063f34a639d9147
Welfegor <46806143+Welfegor@users.noreply.github.com>
2021-12-01 19:09:26 +1000

    Main commit
    Main files upload

--- app/src/main/java/ru/fefu/activitytracker/innerTracker/MainActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/innerTracker/MainActivity.kt
@@ -5,61 +5,70 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.ActivityMainBinding
-import ru.fefu.activitytracker.innerTracker.Fragments.Activity
-import ru.fefu.activitytracker.innerTracker.Fragments.Profile
+import ru.fefu.activitytracker.innerTracker.fragments.ActivityFragment
+import ru.fefu.activitytracker.innerTracker.fragments.ProfileFragment
 
 
-class TrackerActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupActivity(savedInstanceState)
-        setupNavbarHandler()
+
+        binding.navbarTracker.setOnNavigationItemSelectedListener {
+            setupNavHandler(it.itemId)
+            true
+        }
     }
 
     private fun setupActivity(savedInstanceState: Bundle?) {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().apply {
-                add(
-                    R.id.fragment_view_tracker,
-                    Activity.newInstance(),
-                    Activity.tag
-                )
+                add(R.id.tracker_view, ActivityFragment.newInstance(), ActivityFragment.tag,)
                 commit()
             }
         }
     }
 
-    private fun setupNavbarHandler() {
+    private fun setupNavHandler(btn_id: Int) {
         val tabFragments = listOf(
-            TabFragment(R.id.activity, getFragment(Activity.tag),
-                Activity.tag
-            ),
-            TabFragment(R.id.profile, getFragment(Profile.tag),
-                Profile.tag
-            )
-        )
+            TabFragment(R.id.tracker_activity, ActivityFragment.Companion::newInstance, ActivityFragment.tag),
+            TabFragment(R.id.tracker_profile, ProfileFragment.Companion::newInstance, ProfileFragment.tag))
 
-        val navbarHandler = NavbarHandler(tabFragments, supportFragmentManager)
+        val activeFragment = supportFragmentManager.fragments.firstOrNull{!it.isHidden}
+        val hiddenMetaFragment = tabFragments.first { it.buttonId == btn_id }
+        val hiddenFragment = supportFragmentManager.findFragmentByTag(hiddenMetaFragment.tag)
 
-        binding.navbarTracker.setOnItemSelectedListener { item ->
-            navbarHandler.switchFragments(item.itemId)
-            true
+        if (activeFragment == hiddenFragment) return
+        if (hiddenFragment == null) {
+            supportFragmentManager.beginTransaction().apply {
+                add(R.id.tracker_view, hiddenMetaFragment.fragment(), hiddenMetaFragment.tag)
+                commit()
+            }
+        } else {
+            supportFragmentManager.beginTransaction().apply {
+                show(hiddenFragment)
+                commit()
+            }
         }
+        if (activeFragment != null) {
+            supportFragmentManager.beginTransaction().apply {
+                hide(activeFragment)
+                commit()
+            }
+        }
+
     }
 
-    private fun getFragment(tag: String) : Fragment {
-        return supportFragmentManager.findFragmentByTag(tag)
-            ?: when (tag) {
-                Activity.tag -> Activity.newInstance()
-                Profile.tag -> Profile.newInstance()
-                else -> return Fragment()
-            }
+    override fun onBackPressed() {
+        val active = supportFragmentManager.fragments.firstOrNull{!it.isHidden}!!
+        if (active.childFragmentManager.backStackEntryCount != 0) active.childFragmentManager.popBackStack()
     }
 }
 
+
+class TabFragment(val buttonId: Int, val fragment: () -> Fragment, val tag: String)
